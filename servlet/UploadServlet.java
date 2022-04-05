5df87f6b5518f6f27cdc03f7712b1cd1426c25c8
dlj0605 <dlj0605@naver.com>
2022-04-05 15:11:26 +0900

    업로드 중단 예외처리

--- servlet/UploadServlet.java
+++ servlet/UploadServlet.java
@@ -45,7 +45,7 @@ public class UploadServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
     
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException{
 		// TODO Auto-generated method stub
 		String ip = request.getHeader("X-Forwarded-For");
 	    if (ip == null) ip = request.getRemoteAddr();
@@ -69,123 +69,130 @@ public class UploadServlet extends HttpServlet {
 
 		
 		// 이어올리기는 전에 체크 해야된다.
-		
-		InputStream yesno = request.getPart("yesno").getInputStream();
-		InputStreamReader yesnoReader = new InputStreamReader(yesno);
-		Stream<String> yesnoString= new BufferedReader(yesnoReader).lines();
-	    String yn = yesnoString.collect(Collectors.joining());
-
-		//////////////////////// Part&Write ////////////////////////
-		log+="========="+ip+"=========\n";
-		log+="==="+formdatenow+".==\n";
-		Collection<Part> parts = request.getParts();
-		for (Part part : parts) {
-			if(part.getContentType()!=null) {
-				String fileName = part.getName();
+	    try {			    
+			InputStream yesno = request.getPart("yesno").getInputStream();
+			InputStreamReader yesnoReader = new InputStreamReader(yesno);
+			Stream<String> yesnoString= new BufferedReader(yesnoReader).lines();
+		    String yn = yesnoString.collect(Collectors.joining());
+	
+			//////////////////////// Part&Write ////////////////////////
+			log+="========="+ip+"=========\n";
+			log+="==="+formdatenow+".==\n";
+			Collection<Part> parts = request.getParts();
+			for (Part part : parts) {
+				if(part.getContentType()!=null) {
+					String fileName = part.getName();
+					
+					InputStream fnis = request.getPart(fileName+" size").getInputStream();
+					InputStreamReader inputStreamReader = new InputStreamReader(fnis);
+					Stream<String> streamOfString= new BufferedReader(inputStreamReader).lines();
+				    long fileSize = Long.parseLong(streamOfString.collect(Collectors.joining()));
+					
+					//System.out.printf("파라미터 명 : %s, contentType :  %s,  size : %d bytes \n", part.getName(),part.getContentType(), part.getSize());
+					log+="<업로드> "+fileName+"\n";
+	
+					File checkFile = new File(path+"\\"+fileName);
+				    if(yn.equals("NO")&&checkFile.exists()) {
+				    	checkFile.delete();
+				    }
+	
+					if(checkFile.exists()) {
+						FileOutputStream stream = new FileOutputStream(path+"\\"+fileName, true);
+						InputStream is = part.getInputStream();
+				        int read;
+				        byte[] b =new byte[10000];
+				        int bytesBuffered = 0;
+				        while((read = is.read(b,0,b.length))!= -1){
+				        	stream.write(b,0,read);
+				            bytesBuffered += read;
+				            if (bytesBuffered > 1024 * 1024 * 25) {
+				                bytesBuffered = 0;
+				                stream.flush();
+				            }
+				        }
+					    is.close();
+					    stream.close();
+					}
+					else { // notExist
+						part.write(path+"\\"+fileName);
+					}
+	
+					checkFile = new File(path+"\\"+fileName);
+					
+					if(checkFile.length() == fileSize) {
+						Path oldfile = Paths.get(path+"\\"+fileName);
+						Path newfile = Paths.get(realPath+"\\"+fileName);
+						File nf = new File(realPath+"\\"+fileName);
+						int i = 1;
+						String newName = "";
+						while(nf.exists()) {
+							int li = fileName.lastIndexOf(".");
+							newName = fileName.substring(0, li)+"("+i+")"+fileName.substring(li, fileName.length());
+							newfile = Paths.get(realPath+"\\"+newName);
+							nf = new File(realPath+"\\"+newName);
+							i++;
+						}
+						if(newName!="") {log+="└><중복된 파일명 변경> "+newName+"\n";}
+						Files.move(oldfile, newfile, StandardCopyOption.ATOMIC_MOVE);
+						checkFile.delete();
+						log+="=================================";
+						System.out.println(log);
+					}
+				}
+			}
+			
+			////////////////////////////////////////////////////////////
+			
+			/*
+			//////////////////////////// COS ///////////////////////////
+			int size = (1024 * 1024 * 2048)-1;
+			MultipartRequest multi = new MultipartRequest(request, path, size, "UTF-8");
+			Enumeration fileNames = multi.getFileNames();
+			log+="========="+ip+"=========\n";
+			log+="==="+formdatenow+"==\n";
+			String val = "";
+			String fileSize = "";
+			while(fileNames.hasMoreElements()) {
+				val = (String) fileNames.nextElement();
+				log+="<업로드> "+val+"\n";
+				fileSize = multi.getParameter(val+" size");
+				Long longSize = Long.parseLong(fileSize);
+				File checkFile = new File(path+"\\"+val);
 				
-				InputStream fnis = request.getPart(fileName+" size").getInputStream();
-				InputStreamReader inputStreamReader = new InputStreamReader(fnis);
-				Stream<String> streamOfString= new BufferedReader(inputStreamReader).lines();
-			    long fileSize = Long.parseLong(streamOfString.collect(Collectors.joining()));
-				
-				//System.out.printf("파라미터 명 : %s, contentType :  %s,  size : %d bytes \n", part.getName(),part.getContentType(), part.getSize());
-				log+="<업로드> "+fileName+"\n";
-
-				File checkFile = new File(path+"\\"+fileName);
-			    if(yn.equals("NO")&&checkFile.exists()) {
-			    	checkFile.delete();
-			    }
-			    
 				if(checkFile.exists()) {
-					FileOutputStream stream = new FileOutputStream(path+"\\"+fileName, true);
-					InputStream is = part.getInputStream();
-			        int read;
-			        byte[] b =new byte[10000];
-			        int bytesBuffered = 0;
-			        while((read = is.read(b,0,b.length))!= -1){
-			        	stream.write(b,0,read);
-			            bytesBuffered += read;
-			            if (bytesBuffered > 1024 * 1024 * 25) {
-			                bytesBuffered = 0;
-			                stream.flush();
-			            }
-			        }
-			        
-				    is.close();
-				    stream.close();
+					if(checkFile.length() == longSize) {
+						Path oldfile = Paths.get(path+"\\"+val);
+						Path newfile = Paths.get(realPath+"\\"+val);
+						File nf = new File(realPath+"\\"+val);
+						int i = 1;
+						String newName = "";
+						while(nf.exists()) {
+							int li = val.lastIndexOf(".");
+							newName = val.substring(0, li)+"("+i+")"+val.substring(li, val.length());
+							newfile = Paths.get(realPath+"\\"+newName);
+							nf = new File(realPath+"\\"+newName);
+							i++;
+						}
+						if(newName!="") {log+="└><중복된 파일명 변경> "+newName+"\n";}
+						Files.move(oldfile, newfile, StandardCopyOption.ATOMIC_MOVE);
+						checkFile.delete();
+					}
 				}
 				else { // notExist
-					part.write(path+"\\"+fileName);
-				}
-				
-				checkFile = new File(path+"\\"+fileName);
-				
-				if(checkFile.length() == fileSize) {
-					Path oldfile = Paths.get(path+"\\"+fileName);
-					Path newfile = Paths.get(realPath+"\\"+fileName);
-					File nf = new File(realPath+"\\"+fileName);
-					int i = 1;
-					String newName = "";
-					while(nf.exists()) {
-						int li = fileName.lastIndexOf(".");
-						newName = fileName.substring(0, li)+"("+i+")"+fileName.substring(li, fileName.length());
-						newfile = Paths.get(realPath+"\\"+newName);
-						nf = new File(realPath+"\\"+newName);
-						i++;
-					}
-					if(newName!="") {log+="└><중복된 파일명 변경> "+newName+"\n";}
-					Files.move(oldfile, newfile, StandardCopyOption.ATOMIC_MOVE);
-					checkFile.delete();
-					log+="=================================";
-					System.out.println(log);
+					log+="fileNotFound\n";
 				}
 			}
-		}
-		////////////////////////////////////////////////////////////
-		
-		/*
-		//////////////////////////// COS ///////////////////////////
-		int size = (1024 * 1024 * 2048)-1;
-		MultipartRequest multi = new MultipartRequest(request, path, size, "UTF-8");
-		Enumeration fileNames = multi.getFileNames();
-		log+="========="+ip+"=========\n";
-		log+="==="+formdatenow+"==\n";
-		String val = "";
-		String fileSize = "";
-		while(fileNames.hasMoreElements()) {
-			val = (String) fileNames.nextElement();
-			log+="<업로드> "+val+"\n";
-			fileSize = multi.getParameter(val+" size");
-			Long longSize = Long.parseLong(fileSize);
-			File checkFile = new File(path+"\\"+val);
-			
-			if(checkFile.exists()) {
-				if(checkFile.length() == longSize) {
-					Path oldfile = Paths.get(path+"\\"+val);
-					Path newfile = Paths.get(realPath+"\\"+val);
-					File nf = new File(realPath+"\\"+val);
-					int i = 1;
-					String newName = "";
-					while(nf.exists()) {
-						int li = val.lastIndexOf(".");
-						newName = val.substring(0, li)+"("+i+")"+val.substring(li, val.length());
-						newfile = Paths.get(realPath+"\\"+newName);
-						nf = new File(realPath+"\\"+newName);
-						i++;
-					}
-					if(newName!="") {log+="└><중복된 파일명 변경> "+newName+"\n";}
-					Files.move(oldfile, newfile, StandardCopyOption.ATOMIC_MOVE);
-					checkFile.delete();
-				}
-			}
-			else { // notExist
-				log+="fileNotFound\n";
-			}
-		}
-		log+="=================================";
-		System.out.println(log);
-		////////////////////////////////////////////////////////////
-		*/
+			log+="=================================";
+			System.out.println(log);
+			////////////////////////////////////////////////////////////
+			*/
+	    }catch(Exception e) {
+			log+="========="+ip+"=========\n";
+			log+="==="+formdatenow+".==\n";
+	    	log+="============<업로드 중단>===========\n";
+	    	log+="=================================";
+			System.out.println(log);
+	    }
 	}
 
 }
